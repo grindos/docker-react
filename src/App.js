68a3950223fe77a01a7d9cf16d889125b222c4cb
Grigoriy Yanushkovsky <grindosart@gmail.com>
2024-04-12 18:25:12 +0300

    Update text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Text from Feature branch
         </a>
       </header>
     </div>
